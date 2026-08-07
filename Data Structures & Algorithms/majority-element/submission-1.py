8d4db545b634a4942e9e90fe1a69721343b507cb
CHIRDE NIRANJAN ANANT <129379137+niranjan310504@users.noreply.github.com>
2026-08-07 18:49:39 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.py
@@ -0,0 +1,5 @@
+from collections import Counter
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        counts = Counter(nums)
+        return max(counts, key = counts.get)
